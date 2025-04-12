1e332e130a976679a209d3db13f4408c8c0a2df2
Tynets <leshchova2004@gmail.com>
2025-04-12 19:15:43 +0200

    Fixed encrypting

--- keygenerator/src/main/java/socs/keygen/Main.java
+++ keygenerator/src/main/java/socs/keygen/Main.java
@@ -1,13 +1,10 @@
 package socs.keygen;
 
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 import java.security.InvalidKeyException;
-import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -27,16 +24,17 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DocumentFilter;
+import javax.swing.text.PlainDocument;
 
 public class Main {
     private static KeyGenerator keyGen;
     public static void main(String[] args) {
         try {
             keyGen = new KeyGenerator(KeyPairGenerator.getInstance("RSA"), 4096);
-        } catch (NoSuchAlgorithmException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        } catch (NoSuchAlgorithmException e) {}
 
         JFrame frame = new JFrame("RSA key generator");
         frame.setLayout(null);
@@ -47,6 +45,16 @@ public class Main {
         panelPIN.setBounds(20, 20, 150, 20);
         JLabel labelPIN = new JLabel("Enter PIN:");
         JPasswordField fieldPIN = new JPasswordField();
+        PlainDocument document = (PlainDocument) fieldPIN.getDocument();
+        document.setDocumentFilter(new DocumentFilter() {
+            @Override
+            public void replace(DocumentFilter.FilterBypass fb, int offset, int length, String text, AttributeSet attrs) throws BadLocationException {
+                String string = fb.getDocument().getText(0, fb.getDocument().getLength()) + text;
+                if (string.length() <= 4) {
+                    super.replace(fb, offset, length, text, attrs);
+                }
+            }
+        });
         panelPIN.add(labelPIN);
         panelPIN.add(fieldPIN);
 
@@ -63,43 +71,49 @@ public class Main {
         panelButton.setLayout(new BoxLayout(panelButton, BoxLayout.X_AXIS));
         panelButton.setBounds(20, 100, 150, 20);
         JButton buttonGen = new JButton("Generate");
-        buttonGen.addActionListener(e -> {
-            try {
-                buttonGenClicked(fieldPIN.getPassword(), fieldDir.getText(), frame, fieldPIN);
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
-        });
+        buttonGen.addActionListener(e -> buttonGenClicked(fieldPIN.getPassword(), fieldDir.getText(), frame));
         panelButton.add(buttonGen);
-
         frame.add(panelPIN);
         frame.add(panelFile);
         frame.add(panelButton);
         frame.setVisible(true);
     }
 
-    private static void buttonGenClicked(char[] pin, String directory, JFrame frame, JPasswordField fieldPIN) throws IOException {
+    private static void buttonGenClicked(char[] pin, String directory, JFrame frame) {
         keyGen.generateKeyPair();
         StringBuilder sb = new StringBuilder();
-        for (char c : fieldPIN.getPassword()) sb.append(c);
-        byte[] hashPIN = {}, encryptedPrivateKey = {};
+        for (char c : pin) sb.append(c);
+        
+        byte[] encryptedPrivateKey = {};
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            hashPIN = digest.digest(sb.toString().getBytes());
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        // TODO: SecretKeySpec doesn't work for SHA
-        SecretKey key = new SecretKeySpec(hashPIN, 0, hashPIN.length, "SHA-256");
-        Cipher cipher;
-        try {
-            cipher = Cipher.getInstance("AES");
+            byte[] hashPIN = digest.digest(sb.toString().getBytes(StandardCharsets.UTF_8));
+            SecretKey key = new SecretKeySpec(hashPIN, 0, hashPIN.length, "AES");
+            Cipher cipher = Cipher.getInstance("AES");
             cipher.init(Cipher.ENCRYPT_MODE, key);
             encryptedPrivateKey = cipher.doFinal(keyGen.getPrivateKey());
         } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
             e.printStackTrace();
         }
+        
+        try {
+            FileOutputStream outputStream = new FileOutputStream(Paths.get(directory, "key.priv").toString());
+            outputStream.write(encryptedPrivateKey);
+            outputStream.flush();
+            outputStream.close();
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(frame, "Error while writing to file.", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+        try {
+            FileOutputStream outputStream = new FileOutputStream(Paths.get(directory, "key.pub").toString());
+            outputStream.write(keyGen.getPublicKey());
+            outputStream.flush();
+            outputStream.close();
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(frame, "Error while writing to file.", "Error", JOptionPane.ERROR_MESSAGE);
+        }
 
+        /*
         try (FileWriter fileWriter = new FileWriter(Paths.get(directory, "privateKey.txt").toString())) {
             fileWriter.write(keyGen.getKeyHEX(encryptedPrivateKey));
             fileWriter.flush();
@@ -108,6 +122,7 @@ public class Main {
             fileWriter.write(keyGen.getKeyHEX(keyGen.getPublicKey()));
             fileWriter.flush();
         }
+        */
         JOptionPane.showMessageDialog(frame,  "Saved to: " + directory);
     }
 
